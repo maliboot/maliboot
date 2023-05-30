faea0f0ab3b55e9dfe2e612ebd77f3ae0a4f3021
stone <shilei_zhang@163.com>
2023-05-30 10:57:50 +0800

    build(master): 更新构建

--- bin/split.sh
+++ bin/split.sh
@@ -6,34 +6,12 @@ set -x
 # 在目录下运行 ./bin/split.sh (无commit的情况下，无需运行)
 
 CURRENT_BRANCH="master"
-BASEPATH=$(cd `dirname $0`; cd ../; pwd)
+BASEPATH=$(cd `dirname $0`; cd ../src; pwd)
 REPOS=$@
-REPOS_ONLY=(
-api-annotation
-auth
-cola
-contract
-di
-dto
-error-code
-exception-handler
-field-collector
-hashing
-passport
-plugin
-plugin-code-generator
-plugin-config
-request
-response
-response-wrapper
-swagger
-utils
-validation
-)
 
 function split()
 {
-    SHA1=`$BASEPATH/bin/splitsh-lite --prefix=$1`
+    SHA1=`./bin/splitsh-lite --prefix=$1`
     git push $2 "$SHA1:refs/heads/$CURRENT_BRANCH" -f
 }
 
@@ -45,13 +23,10 @@ function remote()
 git pull origin $CURRENT_BRANCH
 #
 if [[ $# -eq 0 ]]; then
-    REPOS=$(ls $BASEPATH/src)
+    REPOS=$(ls $BASEPATH)
 fi
 #
 for REPO in $REPOS ; do
-    if [[ ${REPOS_ONLY[@]/${REPO}/} = ${REPOS_ONLY[@]} ]]; then
-        continue ;
-    fi
     remote $REPO git@github.com:maliboot/$REPO.git
     split "src/$REPO" $REPO
 done
