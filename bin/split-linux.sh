cdaa7bddd16c637def9b2fcd5c4cff28a2292b93
stone <shilei_zhang@163.com>
2023-05-31 11:06:10 +0800

    docs(master): 更新组件描述

---
+++ bin/split-linux.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+# 在目录下运行 ./bin/split.sh (无commit的情况下，无需运行)
+
+CURRENT_BRANCH="master"
+BASEPATH=$(cd `dirname $0`; cd ../src; pwd)
+REPOS=$@
+
+function split()
+{
+    SHA1=`./bin/splitsh-lite-linux --prefix=$1`
+    git push $2 "$SHA1:refs/heads/$CURRENT_BRANCH" -f
+}
+
+function remote()
+{
+    git remote add $1 $2 || true
+}
+
+git pull origin $CURRENT_BRANCH
+#
+if [[ $# -eq 0 ]]; then
+    REPOS=$(ls $BASEPATH)
+fi
+#
+for REPO in $REPOS ; do
+    remote $REPO git@github.com:maliboot/$REPO.git
+    split "src/$REPO" $REPO
+done
