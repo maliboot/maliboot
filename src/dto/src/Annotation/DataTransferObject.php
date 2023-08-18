2ea5399f60f0dc41d73ae2c3748bf2ab09d27f0f
stone <shilei_zhang@163.com>
2023-08-18 15:20:13 +0800

    feat(master): dto.添加相关注解类属性委托功能

--- src/dto/src/Annotation/DataTransferObject.php
+++ src/dto/src/Annotation/DataTransferObject.php
@@ -14,17 +14,31 @@ use MaliBoot\Dto\Contract\QueryDTOAnnotationInterface;
 #[Attribute(Attribute::TARGET_CLASS)]
 class DataTransferObject extends AbstractAnnotation implements BaseDTOAnnotationInterface, QueryDTOAnnotationInterface
 {
+    /**
+     * @param ?string $getterSetterDelegate GetterSetter委托类<div><p>默认为null，无委托</p><p>委托类需要实现<a href='psi_element://\MaliBoot\Lombok\Contract\GetterSetterDelegateInterface'>GetterSetterDelegateInterface</a></p></div>
+     */
     public function __construct(
         public string $name = '',
         public string $desc = '',
         public string $type = '',
         public string $in = RequestParameterLocation::BODY,
-        public string $contentType = ContentType::WWW_FORM_URLENCODED
+        public string $contentType = ContentType::WWW_FORM_URLENCODED,
+        public ?string $getterSetterDelegate = null,
     ) {
     }
 
     public function getType(): string
     {
         return $this->type;
+    }
+
+    public function getterDelegate(): ?string
+    {
+        return $this->getterSetterDelegate;
+    }
+
+    public function setterDelegate(): ?string
+    {
+        return $this->getterSetterDelegate;
     }
 }
--- src/dto/src/Annotation/ViewObject.php
+++ src/dto/src/Annotation/ViewObject.php
@@ -12,7 +12,20 @@ use MaliBoot\Dto\Contract\OfDOAnnotationInterface;
 #[Attribute(Attribute::TARGET_CLASS)]
 class ViewObject extends AbstractAnnotation implements BaseDTOAnnotationInterface, OfDOAnnotationInterface
 {
-    public function __construct(public string $name = '', public string $desc = '')
+    /**
+     * @param ?string $getterSetterDelegate GetterSetter委托类<div><p>默认为null，无委托</p><p>委托类需要实现<a href='psi_element://\MaliBoot\Lombok\Contract\GetterSetterDelegateInterface'>GetterSetterDelegateInterface</a></p></div>
+     */
+    public function __construct(public string $name = '', public string $desc = '', public ?string $getterSetterDelegate = null)
     {
+    }
+
+    public function getterDelegate(): ?string
+    {
+        return $this->getterSetterDelegate;
+    }
+
+    public function setterDelegate(): ?string
+    {
+        return $this->getterSetterDelegate;
     }
 }
