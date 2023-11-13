4b0063dd8503b8f0552a81e263e5fe6cf6f68e4a
stone <shilei_zhang@163.com>
2023-11-13 18:33:32 +0800

    feat(master): dto/vo/do添加arrayObject特性支持

--- src/dto/src/Contract/StructureObjectAnnotationInterface.php
+++ src/dto/src/Contract/StructureObjectAnnotationInterface.php
@@ -4,12 +4,9 @@ declare(strict_types=1);
 
 namespace MaliBoot\Dto\Contract;
 
-use MaliBoot\Lombok\Contract\GetterAnnotationInterface;
-use MaliBoot\Lombok\Contract\OfAnnotationInterface;
-use MaliBoot\Lombok\Contract\SetterAnnotationInterface;
-use MaliBoot\Lombok\Contract\ToArrayAnnotationInterface;
+use MaliBoot\Lombok\Contract\ArrayObjectAnnotationInterface;
 use MaliBoot\Lombok\Contract\ToCollectionAnnotationInterface;
 
-interface StructureObjectAnnotationInterface extends GetterAnnotationInterface, SetterAnnotationInterface, OfAnnotationInterface, ToArrayAnnotationInterface, ToCollectionAnnotationInterface, IsPropertyInitAnnotationInterface, MagicToStringAnnotationInterface
+interface StructureObjectAnnotationInterface extends ArrayObjectAnnotationInterface, ToCollectionAnnotationInterface, IsPropertyInitAnnotationInterface, MagicToStringAnnotationInterface
 {
 }
