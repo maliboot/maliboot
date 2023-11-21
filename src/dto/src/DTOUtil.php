deb19ba6d670254c7c9f0ed354d742e52022b082
stone <shilei_zhang@163.com>
2023-11-21 18:12:39 +0800

    feat(master): dto.添加dto、vo类判断方法

--- src/dto/src/DTOUtil.php
+++ src/dto/src/DTOUtil.php
@@ -5,21 +5,66 @@ namespace MaliBoot\Dto;
 use MaliBoot\Dto\Annotation\DataTransferObject;
 use MaliBoot\Dto\Annotation\ViewObject;
 use ReflectionClass;
+use Hyperf\Collection\Collection;
 
 class DTOUtil
 {
-    public static function isDTO(string $clazz): bool
+    public static function isCollectionAttribute(Collection $collection, array $filterAttributes): bool
+    {
+        $firstItem = self::getCollectionFirstClazz($collection);
+        if ($firstItem === '') {
+            return false;
+        }
+        return self::hasAttribute($firstItem, $filterAttributes);
+    }
+
+    public static function isCollectionDTO(Collection $collection): bool
+    {
+        $firstItem = self::getCollectionFirstClazz($collection);
+        if ($firstItem === '') {
+            return false;
+        }
+        return DTOUtil::isDTO($firstItem);
+    }
+
+    public static function isCollectionVO(Collection $collection): bool
+    {
+        $firstItem = self::getCollectionFirstClazz($collection);
+        if ($firstItem === '') {
+            return false;
+        }
+        return DTOUtil::isVO($firstItem);
+    }
+
+    protected static function getCollectionFirstClazz(Collection $collection): string|object
+    {
+        if ($collection->isEmpty()) {
+            return '';
+        }
+
+        $firstItem = $collection->first();
+        if (! (is_string($firstItem) || is_a($firstItem))) {
+            return '';
+        }
+
+        return $firstItem;
+    }
+
+    public static function isDTO(string|object $clazz): bool
     {
         return self::hasAttribute($clazz, [DataTransferObject::class]);
     }
 
-    public static function isVO(string $clazz): bool
+    public static function isVO(string|object $clazz): bool
     {
         return self::hasAttribute($clazz, [ViewObject::class]);
     }
 
-    public static function hasAttribute(string $clazz, array $filterAttributes): bool
+    public static function hasAttribute(string|object $clazz, array $filterAttributes): bool
     {
+        if (is_string($clazz) && ! class_exists($clazz)) {
+            return false;
+        }
         $class = new ReflectionClass($clazz);
         foreach ($class->getAttributes() as $attribute) {
             if (in_array($attribute->getName(), $filterAttributes)) {
