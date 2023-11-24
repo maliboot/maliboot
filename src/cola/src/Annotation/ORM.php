7f135605a5c50f97d2f6dea988f78f14b86107a2
stone <shilei_zhang@163.com>
2023-11-24 19:08:33 +0800

    feat(master): cola.ORM注解优化

--- src/cola/src/Annotation/ORM.php
+++ src/cola/src/Annotation/ORM.php
@@ -6,10 +6,9 @@ namespace MaliBoot\Cola\Annotation;
 
 use Attribute;
 use MaliBoot\Lombok\Contract\OfAnnotationInterface;
-use MaliBoot\Lombok\Contract\ToArrayAnnotationInterface;
 
 #[Attribute(Attribute::TARGET_PROPERTY)]
-class ORM implements OfAnnotationInterface, ToArrayAnnotationInterface
+class ORM implements OfAnnotationInterface
 {
     /**
      * Laravel-ORM 管理，如字段映射、模型关系定义、模型事件......
